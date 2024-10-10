e072b4a0f1771ee928ea12c7efbb131993d6a2d8
User <ctthien304@gmail.com>
2024-10-11 04:36:16 +0700

    demo

---
+++ onlinePostOfficeManagement/Data/AppDbContextcs.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using onlinePostOfficeManagement.Models;
+
+
+namespace onlinePostOfficeManagement.Data
+{
+    public class AppDbContext :IdentityDbContext<Users>
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
---
+++ onlinePostOfficeManagement/Models/Users.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace onlinePostOfficeManagement.Models
+{
+
+    public class Users : IdentityUser
+    {
+        public string FullName { get; set; }
+    }
+
+}
